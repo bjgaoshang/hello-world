9d1338bef7d89dada02d6bd3a67965b64c6e0e97
Hanhesong <hesong.han@gmail.com>
2017-01-12 22:51:43 +0800

    practice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
+
         System.out.println("hello");
+        System.out.println("1+2=" + (1 + 2));
         System.out.println("hello");
     }
 }
